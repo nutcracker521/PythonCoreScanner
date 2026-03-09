b7c0a249045378d6767abc284851045b9c69a3f0
Jyro Batibot <jyrogodwin.batibot.301058@gmail.com>
2026-03-08 22:03:39 -0700

    finished up on the crawl function

--- scanner.py
+++ scanner.py
@@ -33,6 +33,28 @@ class WebSecurityScanner:
             self.visited_urls.add(url)
             response = self.session.get(url)
             soup = BeautifulSoup(response.text, "html.parser")
+            links = soup.find_all('a', href=True)
+            for link in links:
+                next_url = urllib.parse.urljoin(url, link['href'])
+                if next_url.startswith(self.url):
+                    self.crawl(next_url, depth + 1)
+                    
+        except Exception as e: 
+            print(f"Error crawling {url}: {str(e)}")
+         
+         
+if __name__ == '__main__'   :
+    if len(sys.argv) > 1:
+        url_from_cmd = sys.argv[1]
+        crawl = WebSecurityScanner(url_from_cmd)
+        crawl.crawl(url_from_cmd)
+        print(crawl.visited_urls)
+    else:
+        print("Please provde a name as a command line argument")
+                
+                
+                
+            
             
             
             
